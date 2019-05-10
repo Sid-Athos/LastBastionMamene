db91a870762f6d5a26c7d886cc9470ab018a025a
Fopodir <abidi.feras@hotmail.com>
2019-05-10 10:23:54 +0200

    .Wall

--- LastBastion/LastBastion/Wall.cs
+++ LastBastion/LastBastion/Wall.cs
@@ -6,5 +6,24 @@ namespace LastBastion
 {
     public class Wall : Building
     {
+        uint _rank = 1;
+
+        public Wall(float posX, float posY, uint lifePoints, uint armor,uint rank)
+            : base(posX, posY, lifePoints, armor, rank)
+        {
+
+        }
+
+        public uint Rank => _rank;
+
+        public void Upgrade()
+        {
+            if(_rank < 3)
+            {
+                _rank++;
+                IncHealth();
+                IncreaseArmor();
+            }
+        }
     }
 }
