b8217714144f4c9d0cd9f7d42113417b51223c65
Fopodir <abidi.feras@hotmail.com>
2019-05-24 11:29:25 +0200

    LavaWall

---
+++ LastBastion/LastBastion/LavaWall.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LastBastion
+{
+    public class LavaWall : Wall
+    {
+        uint _rank = 1;
+        uint _dmg;
+        uint _aaCooldown;
+        float _range = 1.0f;
+
+        public LavaWall(float posX, float posY,
+            uint lifePoints, uint maxLifePoints,
+            uint armor, uint rank, uint dmg, float range, uint aaCooldown,
+            uint woodCost, uint foodCost, uint stoneCost, uint villagerCost,
+            Map context, string name, string desc)
+            : base(posX, posY, 200, 200,20,1, 20, 0, 40, 1, context, "LavaWall", "test")
+        {
+            _dmg = dmg;
+            _aaCooldown = aaCooldown;
+            _range = range;
+        }
+    }
+}
